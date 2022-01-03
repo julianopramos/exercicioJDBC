8858a0ea1f928a3e52463aa16641c7a7ec8b6ab5
Juliano Pacheco Ramos <jpachecoramos@gmail.com>
2022-01-03 13:47:45 -0300

    Implementando DaoFactory e SellerDao

--- src/br/com/program/Program.java
+++ src/br/com/program/Program.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import br.com.model.entities.Department;
 import br.com.model.entities.Seller;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 
 public class Program {
 	
@@ -11,6 +13,8 @@ public class Program {
 		
 		Department dp = new Department(1,"Books");
 		Seller seller = new Seller (21, "Bob", "Bob@gmail.com", new Date(), 3000.0, dp);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 		
