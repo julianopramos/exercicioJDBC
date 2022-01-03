d28d7353406211c099698b60da872efce3478bd4
Juliano Pacheco Ramos <jpachecoramos@gmail.com>
2022-01-03 12:23:59 -0300

    Subindo Seller

--- src/br/com/program/Program.java
+++ src/br/com/program/Program.java
@@ -1,14 +1,18 @@
 package br.com.program;
 
+import java.util.Date;
+
 import br.com.model.entities.Department;
+import br.com.model.entities.Seller;
 
 public class Program {
 	
 	public static void main(String[] args) {
 		
 		Department dp = new Department(1,"Books");
+		Seller seller = new Seller (21, "Bob", "Bob@gmail.com", new Date(), 3000.0, dp);
 		
-		System.out.println(dp);
+		System.out.println(seller);
 		
 	}
 }
